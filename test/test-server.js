cbf28ac6df285d85b3217d506b05e297027ed19c
sax0nygerman <i.marx.19@dartmouth.edu>
2017-06-14 17:42:01 -0400

    Add API tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,13 +1,29 @@
 const expect = require('expect.js');
-const http = require('http');
+const request = require('supertest');
+const app = require('../app/server');
 
+const port = process.env.PORT || 9090;
 
-describe('/api', () => {
-  it('should respond to GET with 200', (done) => {
-    http.get('http://localhost:9090/api', (res) => {
-      expect(res).to.exist;
-      expect(res.statusCode).to.equal(200);
+describe('API server', () => {
+  let server;
+  beforeEach(() => {
+    server = app.listen(port);
+  });
+  it('/api returns 200 on GET', (done) => {
+    request(server).get('/api', (done) => {
+      expect(200);
       done();
     });
+    done();
+  });
+  it('/api/users returns 200 on GET', (done) => {
+    request(server).get('/api/users', (done) => {
+      expect(200);
+      done();
+    });
+    done();
+  });
+  afterEach((done) => {
+    server.close(done);
   });
 });
