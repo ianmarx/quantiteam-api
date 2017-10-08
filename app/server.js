d15a39910bd8fc55bc64b9c09ab72d323a905011
ianmarx <i.marx.19@dartmouth.edu>
2017-10-08 15:38:19 -0400

    Add headers to app to fix DELETE error

--- app/server.js
+++ app/server.js
@@ -18,6 +18,22 @@ const app = express();
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
 
+// Add headers
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://qt-dev.surge.sh');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 // enables static assets from folder static
