37dedc81d60e23c6bf3bb31c048a3c069283d4f0
ianmarx <i.marx.19@dartmouth.edu>
2017-10-10 16:24:30 -0400

    test fix for header issue

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import path from 'path';
+// import path from 'path';
 import mongoose from 'mongoose';
 import apiRouter from './router';
 
