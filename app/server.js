cd4a49b51c0a72c3b3477626701c448d52094647
ianmarx <i.marx.19@dartmouth.edu>
2017-10-15 16:11:28 -0400

    test fix

--- app/server.js
+++ app/server.js
@@ -21,12 +21,12 @@ const app = express();
 // Add headers
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
  // res.setHeader('Access-Control-Allow-Credentials', true);
